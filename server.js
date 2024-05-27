a5f4ab0369b6e1c0e0173cbd918adf22a0328d6e
SSTM128 <150303328+SSTM128@users.noreply.github.com>
2024-05-27 18:30:05 +0300

    Made the server run on HTTPS
    added new pem files, that were generated with openssl

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
+const fs = require('fs');
+const https = require('https');
 const app = require('./app');
+
+// Replace with your actual local IP address
+const IP_ADDRESS = '192.168.0.117'; 
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Load SSL certificate and key
+const options = {
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem')
+};
+
+// Create an HTTPS server
+https.createServer(options, app).listen(PORT, IP_ADDRESS, () => {
+  console.log(`Server is running on https://${IP_ADDRESS}:${PORT}`);
+});
